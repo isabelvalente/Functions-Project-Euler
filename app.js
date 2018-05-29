aa6e34b2cc4fa967490d41006cfe8ae31c3d109d
Tech-A33 <tech-a33@Tech-A33s-MacBook-Air.local>
2018-05-30 00:14:15 +0100

    Added last question from lab to end of app.js

--- app.js
+++ app.js
@@ -61,18 +61,46 @@ var fibonacci = function (n)
 
 console.log(fibonacci (35));
 
+// Trying to find the sum pf the even values
+
+// for (var i = 0; i < a.length; i++) {
+//     if(i % 2 === 0) { // index is even
+//         fibonacci.push(a[i]);
+//     }
+// }
+//
+//  //
+//  // for (var i = 0; i < 4000000; i++) {
+//  //   if (fibonnaci) {
+//  //
+//  //
+//  //   }
+//  // }
+
+ // REMAINING ANSWER FOR FUNCTIONS LAB
+
+ // 12. Write a function called 'fizzBuzz' that takes any number and returns a value based on these rules
 
 
-for (var i = 0; i < a.length; i++) {
-    if(i % 2 === 0) { // index is even
-        fibonacci.push(a[i]);
-    }
+
+// But for multiples of three print "Fizz" instead of the number
+// For the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+function fizzBuzz(value){
+
+  switch (true) {
+    case value % 3 == 0 && value % 5 == 0:
+    return value = "FizzBuzz";
+    break;
+    case value % 5 == 0 && value % 3 != 0:
+    return value = "Buzz";
+    break;
+    case value % 3 == 0 && value % 5 != 0:
+    return value = "Fizz";
+    break;
+
+    default:
+    return value;
+  }
 }
-
- //
- // for (var i = 0; i < 4000000; i++) {
- //   if (fibonnaci) {
- //
- //
- //   }
- // }

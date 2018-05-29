1321335da3e01cf864adcdf913581cde9360b748
Tech-A33 <tech-a33@Tech-A33s-MacBook-Air.local>
2018-05-29 23:16:59 +0100

    Solved Fibonacci Sequence

--- app.js
+++ app.js
@@ -1,36 +1,78 @@
-// 1. Write a function called 'multiply' that multiplies two numbers and returns the result
-function multiply () {
+ // PROBLEM 1
 
+ var sum = 0;
+
+
+ for (var i = 0; i < 1000; i++) {
+   if (i % 3 == 0 || i % 5 == 0){
+     sum = sum + i;
+     console.log(i);
+   }
+ }
+ console.log(sum);
+
+
+// PROBLEM 2
+
+// function fibonacci(num){
+//   var a = 1, b = 0, temp;
+//
+//   while(num >= 0){
+//     temp = a;
+//     a = a + b;
+//     b = temp;
+//     num--;
+//   }
+//   return b;
+// }
+
+// function fibonacci(num) {
+//   var sequence = [1, 1];
+//
+//   for (var i = 2; i < num; i++) {
+//     sequence[i] = sequence[i-1] + sequence[i-2];
+//   }
+//   return sequence[number-1];
+// }
+
+// function fibonnaci(num) {
+//   return num < 1 ? 0
+//     :num <=2 ? 1
+//     :fibonacci(num - 1) + fibonacci(num - 2);
+// }
+//
+// console.log(fibonacci(4));
+
+var fibonacci = function (n)
+{
+  if (n===1)
+  {
+    return [0, 1];
+  }
+  else
+  {
+    var s = fibonacci(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+
+
+console.log(fibonacci (35));
+
+
+
+for (var i = 0; i < a.length; i++) {
+    if(i % 2 === 0) { // index is even
+        fibonacci.push(a[i]);
+    }
 }
 
-// 2. Write a function called 'addThree' that adds three numbers together and returns the result
-
-// 3. Write a function called 'smallestNumber' that returns the smaller of 2 numbers
-
-// 4. Write a function called 'maxOfThree' that returns the largest of 3 numbers
-
-// 5. Write a function called 'reverseString' that returns the reverse a string 
-
-// 6. Write a function called 'disemvowel' that returns the vowels from the string parameter
-
-// 7. Write a function called 'removeOdd' that removes all ODD number from an array
-
-// 8. Write a function called 'removeEven' that removes all EVEN number from an array
-
-// 9. Write a function called 'longestString' that takes an array of strings and returns the string with the longest character length
-
-// 10. Write a function called 'allElementsExceptFirstThree' that discards the first 3 elements of an array, 
-// e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
-
-//#### BONUS ####
-
-// 11. Write a function called 'convertArrayToAnObject' that turns an array (with an even number of elements) into a hash, by
-// pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
-// {'a' => 'b', 'c' => 'd'}
-
-// 12. Write a function called 'fizzBuzz' that takes any number and returns a value based on these rules 
-
-// But for multiples of three print "Fizz" instead of the number 
-// For the multiples of five print "Buzz". 
-// For numbers which are multiples of both three and five print "FizzBuzz".
-
+ //
+ // for (var i = 0; i < 4000000; i++) {
+ //   if (fibonnaci) {
+ //
+ //
+ //   }
+ // }
